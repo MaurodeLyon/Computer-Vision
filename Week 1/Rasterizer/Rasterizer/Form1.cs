ec06a2f765783eda0b481d65c079513e0bafc06f
Mauro de Lyon <maurodelyon@gmail.com>
2016-04-18 14:57:35 +0200

    not working properly

---
+++ Week 1/Rasterizer/Rasterizer/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rasterizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Matrix m = Matrix.identity();
+            Matrix m2 = Matrix.identity();
+            Vector3 v = new Vector3(4, 5, 6);
+            Matrix r = m * m2;
+            this.DoubleBuffered = true;
+        }
+        List<Vector3> vertices = new List<Vector3>();
+        List<List<int>> polygons = new List<List<int>>();
+        float rotation = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initialise vertices & polygons here
+            vertices.Add(new Vector3(-1, -1, -1));
+            vertices.Add(new Vector3(1, -1, -1));
+            vertices.Add(new Vector3(1, 1, -1));
+            vertices.Add(new Vector3(-1, 1, -1));
+
+            vertices.Add(new Vector3(-1, -1, 1));
+            vertices.Add(new Vector3(1, -1, 1));
+            vertices.Add(new Vector3(1, 1, 1));
+            vertices.Add(new Vector3(-1, 1, 1));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rotation += 0.01f;
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            
+            g.Clear(Color.LightGray);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Pen pen = new Pen(Color.Black, 2);
+
+            Matrix m = Matrix.identity();
+            m *= Matrix.perspective((float)Math.PI / 2, Width/Height, 0.1f, 20f);
+            m *= Matrix.rotation(rotation, new Vector3(0, 1, 0));
+
+            m *= Matrix.translate(new Vector3(0, 0, -2));
+            foreach (Vector3 v in vertices)
+            {
+                Vector3 p = m * v;
+
+                p.x = Width / 2 + p.x / p.z * Width / 2;
+                p.y = Height / 2 + p.y / p.z * Height / 2;
+
+                g.DrawRectangle(pen, p.x - 5, p.y - 5, 10, 10);
+            }
+        }
+    }
+}
---
+++ Week 1/Rasterizer/Rasterizer/Matrix.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rasterizer
+{
+    struct Matrix
+    {
+        float[,] data;
+
+        private Matrix(int size = 4)
+        {
+            data = new float[size, size];
+        }
+
+
+        public static Matrix identity()
+        {
+            Matrix matrix = new Matrix(4);
+            matrix.data[0, 0] = 1;
+            matrix.data[1, 1] = 1;
+            matrix.data[2, 2] = 1;
+            matrix.data[3, 3] = 1;
+            return matrix;
+        }
+
+        public static Matrix perspective(float fov, float aspect, float zNear, float zFar)
+        {
+            Matrix result = identity();
+
+            result.data[0, 0] = (float)(Math.Atan(fov / 2)) / (aspect);
+            result.data[1, 1] = (float)Math.Atan(fov / 2);
+            result.data[2, 2] = (zFar) / (zFar - zNear);
+            result.data[2, 3] = -1;
+            result.data[3, 2] = (zNear * zFar) / (zFar - zNear);
+            result.data[3, 3] = 0;
+
+            return result;
+        }
+
+        public static Matrix rotation(float angle, Vector3 axis)
+        {
+            Matrix result = identity();
+            float c = (float)Math.Cos(angle);
+            float s = (float)Math.Sin(angle);
+            float x = axis.x;
+            float y = axis.y;
+            float z = axis.z;
+
+            result.data[0, 0] = x * x * (1 - c) + c;
+            result.data[0, 1] = x * y * (1 - c) - z * s;
+            result.data[0, 2] = x * z * (1 - c) + y * s;
+            result.data[0, 3] = 0;
+
+            result.data[1, 0] = x * y * (1 - c) + z * s;
+            result.data[1, 1] = y * y * (1 - c) + c;
+            result.data[1, 2] = y * z * (1 - c) - x * s;
+            result.data[1, 3] = 0;
+
+
+            result.data[2, 0] = x * z * (1 - c) - y * s;
+            result.data[2, 1] = y * z * (1 - c) + x * s;
+            result.data[2, 2] = z * z * (1 - c) + c;
+            result.data[2, 3] = 0;
+
+
+            result.data[3, 0] = 0;
+            result.data[3, 1] = 0;
+            result.data[3, 2] = 0;
+            result.data[3, 3] = 1;
+
+            return result;
+        }
+
+        public static Matrix translate(Vector3 offset)
+        {
+            Matrix result = identity();
+
+            result.data[3, 0] += offset.x;
+            result.data[3, 1] += offset.y;
+            result.data[3, 2] += offset.z;
+            result.data[3, 3] = 1;
+
+            return result;
+        }
+
+        public static Vector3 operator *(Matrix mat, Vector3 vec)
+        {
+            Vector3 result = new Vector3(0, 0, 0);
+
+            result.x = mat.data[0, 0] * vec.x + mat.data[0, 1] * vec.y + mat.data[0, 2] * vec.z + mat.data[0, 3];
+            result.y = mat.data[1, 0] * vec.x + mat.data[1, 1] * vec.y + mat.data[1, 2] * vec.z + mat.data[1, 3];
+            result.z = mat.data[2, 0] * vec.x + mat.data[2, 1] * vec.y + mat.data[2, 2] * vec.z + mat.data[2, 3];
+
+            return result;
+        }
+
+        public static Matrix operator *(Matrix mat1, Matrix mat2)
+        {
+            Matrix matrix = identity();
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    matrix.data[x, y] = (mat1.data[x, 0] * mat2.data[0, y]) + 
+                                        (mat1.data[x, 1] * mat2.data[1, y]) + 
+                                        (mat1.data[x, 2] * mat2.data[2, y]) + 
+                                        (mat1.data[x, 3] * mat2.data[3, y]);
+                }
+            }
+            return matrix;
+        }
+    }
+}
