fd95df2abc21b633b4354be52275d01357039f68
Mauro de Lyon <maurodelyon@gmail.com>
2016-04-25 14:31:30 +0200

    added more grass

--- Les 3/wk2_fps/Minecraft/Main.cpp
+++ Les 3/wk2_fps/Minecraft/Main.cpp
@@ -33,44 +33,45 @@ void drawCube()
 	float y2 = y1 + (1 / 16);
 
 	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, grass_texture_id);
 	glBegin(GL_QUADS);
-	glColor3f(1, 0, 0);
-	glVertex3f(-1, -1, -1);
-	glVertex3f(1, -1, -1);
-	glVertex3f(1, 1, -1);
-	glVertex3f(-1, 1, -1);
+	//glColor3f(1, 0, 0);
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
+	glTexCoord2f(1, 0); glVertex3f(1, -1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, -1);
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, -1);
 
-	glColor3f(1, 1, 0);
-	glVertex3f(-1, -1, 1);
-	glVertex3f(1, -1, 1);
-	glVertex3f(1, 1, 1);
-	glVertex3f(-1, 1, 1);
+	//glColor3f(1, 1, 0);
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, 1);
+	glTexCoord2f(1, 0); glVertex3f(1, -1, 1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, 1);
 
-	glColor3f(0, 0, 1);
-	glVertex3f(-1, -1, -1);
-	glVertex3f(-1, 1, -1);
-	glVertex3f(-1, 1, 1);
-	glVertex3f(-1, -1, 1);
+	//glColor3f(0, 0, 1);
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
+	glTexCoord2f(1, 0); glVertex3f(-1, 1, -1);
+	glTexCoord2f(1, 1); glVertex3f(-1, 1, 1);
+	glTexCoord2f(0, 1); glVertex3f(-1, -1, 1);
 
-	glColor3f(1, -1, 1);
-	glVertex3f(1, -1, -1);
-	glVertex3f(1, 1, -1);
-	glVertex3f(1, 1, 1);
-	glVertex3f(1, -1, 1);
+	//glColor3f(1, -1, 1);
+	glTexCoord2f(0, 0); glVertex3f(1, -1, -1);
+	glTexCoord2f(1, 0); glVertex3f(1, 1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
+	glTexCoord2f(0, 1); glVertex3f(1, -1, 1);
 
-	glColor3f(0, 1, 0);
-	glVertex3f(-1, -1, -1);
-	glVertex3f(1, -1, -1);
-	glVertex3f(1, -1, 1);
-	glVertex3f(-1, -1, 1);
+	//glColor3f(0, 1, 0);
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
+	glTexCoord2f(1, 0); glVertex3f(1, -1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, -1, 1);
+	glTexCoord2f(0, 1); glVertex3f(-1, -1, 1);
 
-	glColor3f(1, 1, 0);
-	glVertex3f(-1, 1, -1);
-	glVertex3f(1, 1, -1);
-	glVertex3f(1, 1, 1);
-	glVertex3f(-1, 1, 1);
+	//glColor3f(1, 1, 0);
+	glTexCoord2f(0, 0); glVertex3f(-1, 1, -1);
+	glTexCoord2f(1, 0); glVertex3f(1, 1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, 1);
 	glEnd();
-	//glDisable(GL_TEXTURE_2D);
+	glDisable(GL_TEXTURE_2D);
 }
 
 void display()
@@ -88,6 +89,7 @@ void display()
 	glRotatef(camera.rotY, 0, 1, 0);
 	glTranslatef(camera.posX, camera.posY, camera.posZ);
 
+	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, grass_texture_id);
 	glBegin(GL_QUADS);
 	glColor3f(1, 1, 1);
